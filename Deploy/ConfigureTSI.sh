92f3617927fc61b047bd61c599650d76ed091fc4
Daisuke Nakahara <daisuken@msn.com>
2021-01-08 13:43:54 -0800

    Update ConfigureTSI.sh

--- Deploy/ConfigureTSI.sh
+++ Deploy/ConfigureTSI.sh
@@ -38,4 +38,8 @@ echo "TSI Client ID : ${temp}"
 temp=$(az webapp config appsettings set --name $webAppName --resource-group $resGroup --settings Azure__TimeSeriesInsights__tsiSecret=$servicePrincipalSecret --query "[?name=='Azure__TimeSeriesInsights__tsiSecret'].[value]" -o tsv)
 echo "TSI Secret    : ${temp}"
 
-temp=$(az timeseriesinsights access-policy create -g $resGroup --environment-name $tsiName -n "TSI-SP" --principal-object-id $servicePrincipalObjectId --roles Reader --only-show-errors)
+temp=$(az timeseriesinsights access-policy list -g $resGroup --environment-name $tsiName --query 'value[].principalObjectId' -o tsv)
+
+if [ -z $temp ]; then
+    temp=$(az timeseriesinsights access-policy create -g $resGroup --environment-name $tsiName -n "TSI-SP" --principal-object-id $servicePrincipalObjectId --roles Reader --only-show-errors)
+fi
